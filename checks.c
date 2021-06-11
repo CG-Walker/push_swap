58a9ecf228274d37089b988ff7c90370160130c9
CG-Walker <cogo97@hotmail.com>
2021-06-11 17:43:40 +0200

    Correction de check_sorted qui ne comparait pas correctement les valeurs

--- checks.c
+++ checks.c
@@ -1,69 +1,69 @@
 #include "push_swap.h"
 
-t_bool  check_sorted(t_list *stack)
+t_bool	check_sorted(t_list *stack)
 {
-    t_list *begin;
+	t_list	*begin;
 
-    begin = stack;
-    while (stack->next != NULL)
-    {
-        if (stack->content > stack->next->content)
-            return (False);
-        stack = stack->next;
-    }
-    stack = begin;
-    return (True);
+	begin = stack;
+	while (stack->next != NULL)
+	{
+		if ((intptr_t)stack->content > (intptr_t)stack->next->content)
+			return (False);
+		stack = stack->next;
+	}
+	stack = begin;
+	return (True);
 }
 
-static t_bool  check_double(char *argv[])
+static t_bool	check_double(char *argv[])
 {
-    size_t i;
-    size_t j;
-    char *tmp;
+	size_t	i;
+	size_t	j;
+	char	*tmp;
 
-    i = 1;
-    j = 1;
-    while (argv[i] != NULL)
-    {
-        tmp = argv[i];
-        j = i + 1;
-        while(argv[j] != NULL)
-        {
-            if (tmp == argv[j])
-                return (True);
-            j++;
-        }
-        i++;
-    }
-    return (False);
+	i = 1;
+	j = 1;
+	while (argv[i] != NULL)
+	{
+		tmp = argv[i];
+		j = i + 1;
+		while (argv[j] != NULL)
+		{
+			if (ft_strcmp(argv[j], tmp) == 0)
+				return (True);
+			j++;
+		}
+		i++;
+	}
+	return (False);
 }
 
-static t_bool  check_args(char *argv[])
+static t_bool	check_args(char *argv[])
 {
-    size_t i;
-    size_t j;
+	size_t	i;
+	size_t	j;
 
-    i = 1;
-    j = 0;
-    while (argv[i])
-    {
-        while (argv[i][j])
-        {
-            if (ft_isdigit(argv[i][j]) == False)
+	i = 1;
+	j = 0;
+	while (argv[i])
+	{
+		while (argv[i][j])
+		{
+			if (argv[i][j] != '-' && ft_isdigit(argv[i][j]) == False)
 				return (False);
-            j++;
-        }
-        j = 0;
-        i++;
-    }
-    return (True);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (True);
 }
 
-t_bool  check_all(char *argv[])
+t_bool	check_all(char *argv[])
 {
-    if (check_args(argv) == False)
-        return (False);
-    if (check_double(argv) == True)
-        return (False);
-    return (True);
+	if (check_args(argv) == False)
+		return (False);
+	if (check_double(argv) == True)
+		return (False);
+	return (True);
 }
