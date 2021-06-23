1d012da3de1c8fa9874f9069c66db94dc2a628b4
CG-Walker <cogo97@hotmail.com>
2021-06-23 17:30:50 +0200

    Ajout des differentes instructions

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: cgoncalv <cgoncalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 14:15:51 by cgoncalv          #+#    #+#             */
-/*   Updated: 2021/06/23 17:04:48 by cgoncalv         ###   ########.fr       */
+/*   Updated: 2021/06/23 17:30:21 by cgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,38 @@
 
 void    do_instr(char *instr, t_list **stack_a, t_list **stack_b)
 {
+    printf("str = %s\n", instr);
     if (ft_strcmp(instr, "sa"))
         do_sa(stack_a, False);
     else if (ft_strcmp(instr, "sb"))
         do_sb(stack_b, False);
-    else if (ft_strcmp(instr, NULL))
+    else if (ft_strcmp(instr, "ss"))
+        do_ss(stack_a, stack_b, False);
+    else if (ft_strcmp(instr, "pa"))
+        do_pa(stack_a, stack_b, False);
+    else if (ft_strcmp(instr, "pb"))
+        do_pb(stack_a, stack_b, False);
+    else if (ft_strcmp(instr, "ra"))
+        do_ra(stack_a, False);
+    else if (ft_strcmp(instr, "rb"))
+        do_rb(stack_b, False);
+    else if (ft_strcmp(instr, "rr"))
+        do_rr(stack_a, stack_b, False);
+    else if (ft_strcmp(instr, "rra"))
+        do_rra(stack_a, False);
+    else if (ft_strcmp(instr, "rrb"))
+        do_rrb(stack_b, False);
+    else if (ft_strcmp(instr, "rrr"))
+        do_rrr(stack_a, stack_b, False);
+    else
     {
+        printf("ENTRY\n");
         if (check_sorted((*stack_a)))
-            printf("Sorted !");
+            printf("OK\n");
         else
-            printf("Not sorted.");
+            printf("KO\n");
     }
-}    
+}
 
 int main(int argc, char *argv[])
 {
